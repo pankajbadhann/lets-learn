e3c03c15d406f6b53e84361b17f7298574dcdae6
pa_kAJ <pankaj4utanda@gmail.com>
2026-06-05 12:32:47 +0530

    write learnwithpankaj in react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        learnwithpankaj
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
